8658b35827031b4f501b0a8ab70f82989566cda2
paddyTea <p.tyrrell2001@gmail.com>
2025-12-03 17:56:12 +1100

    Remove clutter, begin logic to check if song it playing. Currently fucntional if chrome is up to date

--- scrape.py
+++ scrape.py
@@ -1,14 +1,7 @@
-# from selenium import webdriver
 import undetected_chromedriver as uc
-
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-
-from bs4 import BeautifulSoup
-import pickle
-# import time
 from selenium.webdriver.common.by import By
-# import csv
+import csv
+import pandas as pd
 
 options = uc.ChromeOptions()
 options.headless = False
@@ -21,32 +14,32 @@ filename = "TripleM"
 with driver:
     driver.get("https://play.listnr.com/station/mmm-sydney?referrer=triplem&_gl=1*11ucxey*_ga*NjcwNTY4MTM3LjE3NjI1ODM3Mzg.*_ga_FYX3DQJM1Q*czE3NjQyODgzNDgkbzIkZzAkdDE3NjQyODgzNDgkajYwJGwwJGgw")
 
-artist = "dnw"
 track = driver.find_element(By.CLASS_NAME,"funuFA")
 song = track.find_element(By.CLASS_NAME, "gjxUCu").text
 artist = track.find_element(By.CLASS_NAME, "dFNMHX").text
-
-
-print(track)
-print(song)
-print(artist)
-
-
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-try:
-    with open(filename, "wb") as file:
-        pickle.dump(soup, file)
-    print("Successful Dump", filename)
-except Exception as e:
-    print("Error during pickling object (Possibly unsupported):", {e})
-
-# file = open("MyCSVFile.csv","w")
-# writer = csv.writer(file)
-# writer.writerow(["test"])
-# file.close()
-
-# print(soup.prettify())
-# print(filename)
 driver.close()
+
+songAndArtist = song + ": " + artist
+
+if songAndArtist != "Triple M" :
+    
+    df = pd.read_csv("MyCSVFile.csv")
+    last_row = df.tail(1)
+    print(last_row)
+    # if(last_row contains songAndArtist)
+    
+
+    # print(songAndArtist)
+    # file = open("MyCSVFile.csv","r")
+    # csvreader = csv.reader(file)
+    # for row in csvreader:
+    #     print(row)
+    # file.close()
+
+    # if songAndArtist != 
+    
+    file = open("MyCSVFile.csv","a")
+    writer = csv.writer(file)
+    writer.writerow([songAndArtist])
+    file.close()
 
