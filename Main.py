4ec013f5deecad6e9895536380f9c1abaef55a5b
paddyTea <p.tyrrell2001@gmail.com>
2025-12-05 10:44:33 +1100

    Introduce schedule logic to run script every 2 minutes

--- Main.py
+++ Main.py
@@ -18,4 +18,10 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f"Hello world, {bot.user.name}")
 
+
+# @bot.event
+# async def on_message(song):
+#     await message.channel.send(song)
+
+
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
--- scrape.py
+++ scrape.py
@@ -2,51 +2,58 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 import csv
 import pandas as pd
+import schedule
+import time
 
-options = uc.ChromeOptions()
-options.headless = False
 
-
-driver = uc.Chrome(options=options)  
-
-filename = "TripleM"
-
-with driver:
-    driver.get("https://play.listnr.com/station/mmm-sydney?referrer=triplem&_gl=1*11ucxey*_ga*NjcwNTY4MTM3LjE3NjI1ODM3Mzg.*_ga_FYX3DQJM1Q*czE3NjQyODgzNDgkbzIkZzAkdDE3NjQyODgzNDgkajYwJGwwJGgw")
-
-track = driver.find_element(By.CLASS_NAME,"funuFA")
-song = track.find_element(By.CLASS_NAME, "gjxUCu").text
-artist = track.find_element(By.CLASS_NAME, "dFNMHX").text
-driver.close()
-
-songAndArtist = song + ": " + artist
-
-# songAndArtist = "Rain Dragon"
-
-if songAndArtist.strip() != "Triple M Sydney 104.9:":
-    print("Song playing")
-    df = pd.read_csv("MyCSVFile.csv")
-    last_row = df.tail(1)
-    prevSong = last_row.iloc[0,0]
+def scan():
     
-    if songAndArtist != prevSong:
-        print("Song is not last recorded")
+    options = uc.ChromeOptions()
+    options.headless = False
+    driver = uc.Chrome(options=options)  
+
+    filename = "TripleM"
+
+    with driver:
+        driver.get("https://play.listnr.com/station/mmm-sydney?referrer=triplem&_gl=1*11ucxey*_ga*NjcwNTY4MTM3LjE3NjI1ODM3Mzg.*_ga_FYX3DQJM1Q*czE3NjQyODgzNDgkbzIkZzAkdDE3NjQyODgzNDgkajYwJGwwJGgw")
+
+    track = driver.find_element(By.CLASS_NAME,"funuFA")
+    song = track.find_element(By.CLASS_NAME, "gjxUCu").text
+    artist = track.find_element(By.CLASS_NAME, "dFNMHX").text
+    driver.close()
+
+    songAndArtist = song + ": " + artist
+
+    print(songAndArtist)  
+
+    if songAndArtist.strip() != "Triple M Sydney 104.9:":
+        print("Song playing")
+        df = pd.read_csv("MyCSVFile.csv")
+        last_row = df.tail(1)
+        prevSong = last_row.iloc[0,0]
         
-        #Condition if duplicate is found, Put bot message here
-        if df['Songs'].str.contains(songAndArtist).any():
-            print('Song has been played today')
-            #bot message loguic here
+        if songAndArtist != prevSong:
+            print("Song is not last recorded")
+            
+            #Condition if duplicate is found, Put bot message here
+            if df['Songs'].str.contains(songAndArtist).any():
+                print('Song has been played today')
+                #bot message loguic here
+            else:
+                print("Song has not been played today")
+                file = open("MyCSVFile.csv","a")
+                writer = csv.writer(file)
+                writer.writerow([songAndArtist])
+                file.close()
+        
         else:
-            print("Song has not been played today")
-            file = open("MyCSVFile.csv","a")
-            writer = csv.writer(file)
-            writer.writerow([songAndArtist])
-            file.close()
-    
+            print("Song is last recorded")
+
     else:
-        print("Song is last recorded")
+        print("Song is not playing")
 
-else:
-    print("Song is not playing")
+schedule.every(2).minutes.do(scan)
 
-    
+while True:
+    schedule.run_pending()
+    time.sleep(1)
