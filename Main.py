cd66bed1c97a870ce1d0f4da6bdddef2ace9364e
paddyTea <p.tyrrell2001@gmail.com>
2025-12-05 17:06:44 +1100

    bot logic to successfully send a message in server, start logic to call event from scrape file

--- Main.py
+++ Main.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+channelID = int(os.getenv('DISCORD_CHANNEL_ID'))
+print(type(channelID))
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
@@ -14,14 +17,22 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+async def send_message():
+
+    await bot.wait_until_ready()
+
+    await asyncio.sleep(5)
+
+    channel = bot.get_channel(channelID)
+    if channel:
+        await channel.send("This is the message to send")
+
+
 @bot.event
 async def on_ready():
     print(f"Hello world, {bot.user.name}")
-
-
-# @bot.event
-# async def on_message(song):
-#     await message.channel.send(song)
+    bot.loop.create_task(send_message())
 
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
