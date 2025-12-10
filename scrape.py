d87d64f91e890e90fc8d0170f8672c652281c598
paddyTea <p.tyrrell2001@gmail.com>
2025-12-10 18:37:11 +1100

    CSV genreates daily, Code is functional

--- scrape.py
+++ scrape.py
@@ -8,63 +8,66 @@ from datetime import datetime
 from pathlib import Path
 
 now = datetime.now()
-date_time = now.strftime("%m/%d/%Y")
+date_time = now.strftime("%m-%d-%Y")
 csvfile = Path("TripleM " + date_time)
 
 if not csvfile.is_file():
     print("enter loop")
     file = open(csvfile,"w")
     writer = csv.writer(file)
-    writer.writerow("Songs")
+    writer.writerow(["Songs"])
     file.close()
 else:
     print("did not enter")
 
+
+def addSongToCSV(songAndArtist) :
+    print("Song has not been played today")
+    file = open(csvfile,"a")
+    writer = csv.writer(file)
+    writer.writerow([songAndArtist])
+    file.close()
+
 # def scan():
-    
-#     options = uc.ChromeOptions()
-#     options.headless = False
-#     driver = uc.Chrome(options=options)
+options = uc.ChromeOptions()
+options.headless = False
+driver = uc.Chrome(options=options)
 
-#     with driver:
-#         driver.get("https://play.listnr.com/station/mmm-sydney?referrer=triplem&_gl=1*11ucxey*_ga*NjcwNTY4MTM3LjE3NjI1ODM3Mzg.*_ga_FYX3DQJM1Q*czE3NjQyODgzNDgkbzIkZzAkdDE3NjQyODgzNDgkajYwJGwwJGgw")
+with driver:
+    driver.get("https://play.listnr.com/station/mmm-sydney?referrer=triplem&_gl=1*11ucxey*_ga*NjcwNTY4MTM3LjE3NjI1ODM3Mzg.*_ga_FYX3DQJM1Q*czE3NjQyODgzNDgkbzIkZzAkdDE3NjQyODgzNDgkajYwJGwwJGgw")
 
-#     track = driver.find_element(By.CLASS_NAME,"funuFA")
-#     song = track.find_element(By.CLASS_NAME, "gjxUCu").text
-#     artist = track.find_element(By.CLASS_NAME, "dFNMHX").text
-#     driver.close()
+track = driver.find_element(By.CLASS_NAME,"funuFA")
+song = track.find_element(By.CLASS_NAME, "gjxUCu").text
+artist = track.find_element(By.CLASS_NAME, "dFNMHX").text
+driver.close()
 
-#     songAndArtist = song + ": " + artist
+songAndArtist = song + ": " + artist
 
-#     print(songAndArtist)
-    
+print(songAndArtist)
+
+if songAndArtist.strip() != "Triple M Sydney 104.9:":
+    print("Song playing")
+    df = pd.read_csv(csvfile)
+    if len(df)>0 :
+        last_row = df.tail(1)
+        prevSong = last_row.iloc[0,0]
         
-
-#     if songAndArtist.strip() != "Triple M Sydney 104.9:":
-#         print("Song playing")
-#         df = pd.read_csv(csvfile)
-#         last_row = df.tail(1)
-#         prevSong = last_row.iloc[0,0]
-        
-#         if songAndArtist != prevSong:
-#             print("Song is not last recorded")
+        if songAndArtist != prevSong:
+            print("Song is not last recorded")
             
-#             #Condition if duplicate is found, Put bot message here
-#             if df['Songs'].str.contains(songAndArtist).any():
-#                 print('Song has been played today')
-#                 #bot message loguic here
-#             else:
-#                 print("Song has not been played today")
-#                 file = open(csvfile,"a")
-#                 writer = csv.writer(file)
-#                 writer.writerow([songAndArtist])
-#                 file.close()
+            #Condition if duplicate is found, Put bot message here
+            if df['Songs'].str.contains(songAndArtist).any():
+                print('Song has been played today')
+                #bot message loguic here
+            else:
+                addSongToCSV(songAndArtist)
         
-#         else:
-#             print("Song is last recorded")
-
-#     else:
-#         print("Song is not playing")
+        else:
+            print("Song is last recorded")
+    else:
+        addSongToCSV(songAndArtist)
+else:
+    print("Song is not playing")
 
 # schedule.every(2).minutes.do(scan)
 
